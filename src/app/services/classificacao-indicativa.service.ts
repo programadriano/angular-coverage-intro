888122393253ddf690f80eacfd683e44908b82f5
Thiago da Silva Adriano <tadriano.net@gmail.com>
2021-06-26 20:43:14 -0300

    init

--- src/app/services/classificacao-indicativa.service.ts
+++ src/app/services/classificacao-indicativa.service.ts
@@ -7,16 +7,16 @@ export class ClassificacaoIndicativaService {
 
   constructor() { }
 
-  // verificaClassificacaoIndicativaPorIdade(livre: boolean, data_nascimento: Date, idade: number): boolean {
-  //   if (livre) {
-  //     return true;
-  //   } else {
-  //     let birthday = data_nascimento.toString().split("/");
-  //     let day = Number(birthday[1]);
-  //     let month = Number(birthday[0]);
-  //     let year = Number(birthday[2]);
-  //     return new Date(year + idade, month - 1, day) <= new Date();
-  //   }
-  // }
+  verificaClassificacaoIndicativaPorIdade(livre: boolean, data_nascimento: Date, idade: number): boolean {
+    if (livre) {
+      return true;
+    } else {
+      let birthday = data_nascimento.toString().split("/");
+      let day = Number(birthday[1]);
+      let month = Number(birthday[0]);
+      let year = Number(birthday[2]);
+      return new Date(year + idade, month - 1, day) <= new Date();
+    }
+  }
 }
 
